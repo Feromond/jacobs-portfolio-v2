17de7389713925900b25c6966f695c396275f1b1
Feromond <Jacobpmish@gmail.com>
2024-07-22 13:12:32 -0600

    Added experience data to the data file.

--- src/library/data.ts
+++ src/library/data.ts
@@ -56,6 +56,63 @@ export const socialLinks = [
   },
 ]
 
+export const experiences = [
+  {
+    title: 'Data Developer',
+    company: 'Strathcona Resources LTD.',
+    duration: 'March 2023 - Present',
+    description: [
+      'Utilized LLM Transformers to create a Q&A chatbot with contextual knowledge of company HR documentation.',
+      'Developed a web app featuring Azure Single Sign-On authentication, managed group-based permissions, and integration with multiple APIs, centralizing access to diverse tools for enhanced user experience.',
+      'Utilize various REST APIs to collect data, automate tasks, and gather insights for business intelligence purposes.',
+      'Design and develop curated tables from raw data to provide valuable insights for various teams across the organization.',
+      'Create automated workflows for efficient data collection, cleaning, and transformation, generating curated data ready for analysis and visualization.',
+      'Optimize cloud compute costs by configuring ideal settings for specific tasks and improving script runtime efficiency.',
+      'Implement data validation, data transformation, and data quality checks to ensure data consistency and accuracy.',
+      'Stay current with industry trends and best practices in data engineering and technologies to continuously improve data infrastructure and workflows.',
+    ],
+  },
+  {
+    title: 'AI/Machine Learning Consultant (Contract)',
+    company: 'CircutStream',
+    duration: 'October 2023 - Present',
+    description: [
+      'Spearheaded the creation of the capstone project, a sophisticated Discord bot that integrates OpenAI APIs and traditional machine learning models, like recommender systems.',
+      'Collaborated in the development of a comprehensive Machine Learning Pre-University Course, focusing on practical, student-centered learning',
+      'Assisted in the meticulous planning of the course schedule, ensuring a coherent and progressive learning experience',
+      'Played a key role in curating and determining the course content for each session, aligning theoretical knowledge with practical application',
+      'Engineered the Discord bot to offer interactive and personalized topic recommendations, enhancing user engagement and the learning experience',
+      'Continuously validated and refined course content and the final project, guaranteeing accuracy, relevance, and high educational value.',
+    ],
+  },
+  {
+    title: 'Associate Machine Learning Developer (4-Month Contract)',
+    company: 'AltaML',
+    duration: 'September 2022 - December 2022',
+    description: [
+      'Implemented tree-based regression algorithms for predicting the permeability of rock core images with an accuracy of 94%, saving the client upwards of $10 million dollars.',
+      'Conducted error analysis to further improve model performance.',
+      'Prepared visualizations and presented weekly updates to clients.',
+      'Developed a Machine Learning pipeline from scratch on Azure.',
+      'Competed in a Microsoft-hosted hackathon, and created an application utilizing Natural Language Processing (NLP) to extract moods from journals.',
+    ],
+  },
+  {
+    title: 'Undergraduate Researcher',
+    company: 'University Of Calgary',
+    duration: 'December 2020 – February 2022',
+    description: [
+      'Published and presented as primary speaker at EAGE Edinburgh',
+      'Used Machine Learning / Convolutional Neural Networks (CNN’s) from Python Tensorflow to Detect Near Surface Geohazards from Distributed Acoustic Sensing (DAS) Data',
+      'Utilized signal processing techniques to filter and process raw data',
+      'Worked with AWS cloud computing to store and handle large data volumes',
+      'Generated figures and graphics to display analysis and results',
+      'Built interpretation of results from statistical analysis',
+      'Published in Near Surface Geoscience Conference & Exhibition',
+    ],
+  },
+]
+
 export const navbarData = {
   navigator: [
     { name: 'Home' },
