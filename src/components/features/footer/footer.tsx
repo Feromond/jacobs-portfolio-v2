ba058a99731c2e4f24309571b97cacbddd6dd462
Jacob Mish <Jacobpmish@gmail.com>
2025-03-21 14:51:23 -0600

    Updated year in footer

--- src/components/features/footer/footer.tsx
+++ src/components/features/footer/footer.tsx
@@ -2,5 +2,5 @@ import React from 'react'
 import './style.css'
 
 export function Footer() {
-  return <footer className="footer">&#169; 2024 Jacob Mish</footer>
+  return <footer className="footer">&#169; 2025 Jacob Mish</footer>
 }
